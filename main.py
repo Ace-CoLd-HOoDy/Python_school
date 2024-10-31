8402ab66711c1dceb6a105dfaf68d038945d22b0
Ace-CoLd-HOoDy <121887597+Ace-CoLd-HOoDy@users.noreply.github.com>
2024-10-31 14:54:45 +0000

    Task 2

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-x = int(input("Enter any number: "))
-if x >= -1 and x <= 17:
-    print("Входить")
-else:
-    print("Не входить")
+# task 1
+
+# while True:
+#     x = int(input("Enter any number: "))
+#     if x >= -1 and x <= 17:
+#         print("Входить")
+#     else:
+#         print("Не входить")
+
+# task 2
+
+while True:
+    x = int(input("Enter any number: "))
+    if x <= -3 or x >= 7:
+        print("Входить")
+    else: 
+        print("Не входить")
+        break
