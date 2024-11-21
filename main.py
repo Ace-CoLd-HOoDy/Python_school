8d7d4c541111e21ef013ae46161bc15737578556
Ace-CoLd-HOoDy <121887597+Ace-CoLd-HOoDy@users.noreply.github.com>
2024-11-21 16:57:31 +0200

    more for

--- main.py
+++ main.py
@@ -18,12 +18,21 @@
 #         break
 
 # task 7
-x = int(input("Enter any 4 digit number: "))
-if 1000 <= x <= 9999:
-    if x % 7 == 0 or x % 17 == 0:
-        print("Beautiful")
-    else:
-        print("Not beautiful")
+# x = int(input("Enter any 4 digit number: "))
+# if 1000 <= x <= 9999:
+#     if x % 7 == 0 or x % 17 == 0:
+#         print("Beautiful")
+#     else:
+#         print("Not beautiful")
 
-else:
-    print("Not a 4 digit number")
+# else:
+#     print("Not a 4 digit number")
+
+# text = input("Write something: ")
+# amount = int(input("Write how many times will the text repear itself: "))
+# for i in range(amount):
+#     print(text)
+
+text = input("Write something: ")
+for i in range(10):
+    print(i, text)
